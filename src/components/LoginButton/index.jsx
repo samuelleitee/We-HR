aab3acfc3dcdf0f5a1c844309d94b25a27ee27c7
programmer.and.investor@gmail.com <programmer.and.investor@gmail.com>
2024-04-08 17:13:03 -0300

    feat: add type property to LoginButton component
     Co-authored-by: PauloBezz <paulo.bezerra2057@gmail.com>

--- src/components/LoginButton/index.jsx
+++ src/components/LoginButton/index.jsx
@@ -1,8 +1,8 @@
 import styles from "./styles.module.css"
 
-export function LoginButton({ text, onClick, dark }, ...props) {
+export function LoginButton({ text, onClick, dark, type }, ...props) {
     return (
-        <button className={`${styles.button} ${dark ? styles.dark : styles.light}`} onClick={onClick} {...props}>
+        <button className={`${styles.button} ${dark ? styles.dark : styles.light}`} onClick={onClick} type={type} {...props}>
             <span>{text}</span>
         </button>
     )
