dab2e16dd5d8cf32140c03cb279ef1f817b441c9
programmer.and.investor@gmail.com <programmer.and.investor@gmail.com>
2024-04-08 17:13:41 -0300

    feat: add components and elements to Login page
     Co-authored-by: PauloBezz <paulo.bezerra2057@gmail.com>

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import { Input } from "../../components/Input"
+import { LoginButton } from "../../components/LoginButton";
 
 export function Login() {
   return (
-    <>
-    </>
+    <main>
+      <section></section>
+      
+      <section>
+        <form action="">
+          <Input name="email" type="email" placeholder="example@example"/>
+          <Input name="password" type="password" placeholder="8 - 10 characters" />
+          <LoginButton text="LOGIN" dark type="submit" />
+          <LoginButton text="FORGET PASSWORD?" type="button"/>
+        </form>
+      </section>
+    </main>
   );
 }
