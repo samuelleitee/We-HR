6f259ffb51f54436b845ecaa4b8513aaafb9b42f
programmer.and.investor@gmail.com <programmer.and.investor@gmail.com>
2024-04-08 17:10:29 -0300

    feat: add icons to the Input component
     Co-authored-by: PauloBezz <paulo.bezerra2057@gmail.com>

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import styles from "./styles.module.css";
 
+import emailIcon from "../../assets/email.svg"
+import passwordIcon from "../../assets/password.svg"
+
 export function Input({ name, type, placeholder }) {
   return (
     <>
       <label className={styles.label} htmlFor={name}>{name}</label>
-      <input className={styles.input} id={name} name={name} type={type} placeholder={placeholder} />
+
+      <div className={styles.container}>
+        <img src={type == "email" ? emailIcon : passwordIcon} />
+        <input className={styles.input} id={name} name={name} type={type} placeholder={placeholder} />
+      </div>
     </>
   );
 }
