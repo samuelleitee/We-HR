21e569131010bcc426e89c5c4f99b16161289317
programmer.and.investor@gmail.com <programmer.and.investor@gmail.com>
2024-04-08 15:45:27 -0300

    git commit -m "feat: add elements to the LoginButton component
    Co-authored-by: PauloBezz <paulo.bezerra2057@gmail.com>

--- src/components/LoginButton/index.jsx
+++ src/components/LoginButton/index.jsx
@@ -1,5 +1,9 @@
-export function LoginButton() {
-    return(
-        <></>
+import styles from "./styles.module.css"
+
+export function LoginButton({ text, onClick, dark }, ...props) {
+    return (
+        <button className={`${styles.button} ${dark ? styles.dark : styles.light}`} onClick={onClick} {...props}>
+            <span>{text}</span>
+        </button>
     )
 }
