53b0de9a45857bd3d9aa3792c20c742893c6aea6
samuelleitee <programmer.and.investor@gmail.com>
2024-04-10 20:20:55 -0300

    style: add styles to navbar elements according to pathname
    Co-authored-by: PauloBezz <paulo.bezerra2057@gmail.com>

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,45 +1,85 @@
 import { Link } from "react-router-dom";
 import { TextLogo } from "../TextLogo";
 
+import DashboardIcon from "../../assets/ic_dashboard.svg"
+import DashboardIconActive from "../../assets/ic_dashboard_active.svg"
+import RecruitmentIcon from "../../assets/ic_recruitment.svg"
+import RecruitmentIconActive from "../../assets/ic_recruitment_active.svg"
+import ScheduleIcon from "../../assets/ic_calendar.svg"
+import ScheduleIconActive from "../../assets/ic_calendar_active.svg"
+import EmployeeIcon from "../../assets/ic_employee.svg"
+import EmployeeIconActive from "../../assets/ic_employee_active.svg"
+import DepartmentIcon from "../../assets/ic_department.svg"
+import DepartmentIconActive from "../../assets/ic_department_active.svg"
+import SupportIcon from "../../assets/ic_support.svg"
+import SupportIconActive from "../../assets/ic_support_active.svg"
+import SettingsIcon from "../../assets/ic_settings.svg"
+import SettingsIconActive from "../../assets/ic_settings_active.svg"
+
 import styles from "./styles.module.css"
 
 export function Sidebar() {
+    const { pathname } = window.location
+
     return (
         <aside className={styles.sidebar}>
             <nav>
-                <TextLogo />
+                <Link className={styles.logo} to="/dashboard">
+                    <TextLogo />
+                </Link>
 
                 <section>
-                    <header>MAIN MENU</header>
-
+                    <header className={styles.header}>MAIN MENU</header>
                     <ul>
-                        <li>
-                            <Link to="/dashboard">Dashboard</Link>
-                        </li>
-                        <li>
-                            <Link to="/dashboard">Recruitment</Link>
-                        </li>
-                        <li>
-                            <Link to="/dashboard">Schedule</Link>
-                        </li>
-                        <li>
-                            <Link to="/dashboard">Employee</Link>
-                        </li>
-                        <li>
-                            <Link to="/dashboard">Department</Link>
-                        </li>
+                        <Link to="/dashboard">
+                            <li className={pathname.split("/")[1] === "dashboard" ? styles.active : null}>
+                                <img src={pathname.split("/")[1] === "dashboard" ? DashboardIconActive : DashboardIcon} />
+                                Dashboard
+                            </li>
+                        </Link>
+                        <Link to="/dashboard">
+                            <li className={pathname.split("/")[1] === "recruitment" ? styles.active : null}>
+                                <img src={pathname.split("/")[1] === "recruitment" ? RecruitmentIconActive : RecruitmentIcon} />
+                                Recruitment
+                            </li>
+                        </Link>
+
+                        <Link to="/dashboard">
+                            <li className={pathname.split("/")[1] === "schedule" ? styles.active : null}>
+                                <img src={pathname.split("/")[1] === "schedule" ? ScheduleIconActive : ScheduleIcon} />
+                                Schedule
+                            </li>
+                        </Link>
+                        <Link to="/dashboard">
+                            <li className={pathname.split("/")[1] === "employee" ? styles.active : null}>
+                                <img src={pathname.split("/")[1] === "employee" ? EmployeeIconActive : EmployeeIcon} />
+                                Employee
+                            </li>
+                        </Link>
+                        <Link to="/dashboard">
+                            <li className={pathname.split("/")[1] === "department" ? styles.active : null}>
+                                <img src={pathname.split("/")[1] === "department" ? DepartmentIconActive : DepartmentIcon} />
+                                Department
+                            </li>
+                        </Link>
                     </ul>
                 </section>
                 <section>
-                    <header>OTHER</header>
+                    <header className={styles.header}>OTHER</header>
 
                     <ul>
-                        <li>
-                            <Link to="/dashboard">Support</Link>
-                        </li>
-                        <li>
-                            <Link to="/dashboard">Settings</Link>
-                        </li>
+                        <Link to="/dashboard">
+                            <li className={pathname.split("/")[1] === "support" ? styles.active : null}>
+                                <img src={pathname.split("/")[1] === "support" ? SupportIconActive : SupportIcon} />
+                                Support
+                            </li>
+                        </Link>
+                        <Link to="/dashboard">
+                            <li className={pathname.split("/")[1] === "settings" ? styles.active : null}>
+                                <img src={pathname.split("/")[1] === "settings" ? SettingsIconActive : SettingsIcon} />
+                                Settings
+                            </li>
+                        </Link>
                     </ul>
                 </section>
             </nav>
