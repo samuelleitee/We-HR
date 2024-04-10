a09db10b7f65fd4cd50421c9da1a6bacf5cfdfd6
samuelleitee <programmer.and.investor@gmail.com>
2024-04-09 22:26:35 -0300

    feat: add properties, elements and styles to Login page
    Co-authored-by: PauloBezz <paulo.bezerra2057@gmail.com>

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,20 +1,75 @@
-import React from "react";
 import { Input } from "../../components/Input"
 import { LoginButton } from "../../components/LoginButton";
 
+import { Routes, Route, useNavigation } from "react-router-dom"
+import { Logo } from "../../components/Logo";
+
+import styles from "./styles.module.css"
+
 export function Login() {
   return (
-    <main>
+    <main className={styles.container}>
       <section></section>
-      
+
       <section>
-        <form action="">
-          <Input name="email" type="email" placeholder="example@example"/>
-          <Input name="password" type="password" placeholder="8 - 10 characters" />
-          <LoginButton text="LOGIN" dark type="submit" />
-          <LoginButton text="FORGET PASSWORD?" type="button"/>
-        </form>
+        <Logo />
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="/password-recovery" element={<PasswordRecoveryForm />} />
+        </Routes>
       </section>
     </main>
   );
+}
+
+function LoginForm() {
+  // const navigation = useNavigation();
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+
+  }
+
+  const handleNavigate = () => {
+    // navigation.navigate("/password-recovery")
+  }
+
+  return (
+    <form action="">
+      <Input name="email" type="email" placeholder="example@example" />
+      <Input name="password" type="password" placeholder="8 - 10 characters" />
+
+      <div className={styles.buttonContainer}>
+        <LoginButton text="LOGIN" dark type="submit" onClick={handleSubmit} />
+        <LoginButton text="FORGET PASSWORD?" type="button" onClick={handleNavigate} />
+      </div>
+    </form>
+  )
+}
+
+function PasswordRecoveryForm() {
+  // const navigation = useNavigation();
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+
+
+  }
+
+  const handleNavigate = () => {
+    // navigation.navigate("/login")
+  }
+
+  return (
+    <form action="">
+      <Input name="email" type="email" placeholder="example@example" />
+      <Input name="password" type="password" placeholder="8 - 10 characters" />
+
+      <div className={styles.buttonContainer}>
+        <LoginButton text="RECOVER PASSWORD" dark type="submit" onClick={handleSubmit} />
+        <LoginButton text="BACK TO LOGIN" type="button" onClick={handleNavigate} />
+      </div>
+    </form>
+  )
 }
