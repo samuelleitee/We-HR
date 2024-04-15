9f1130bc5e079da14a9108ecc17fcec015d01814
samuelleitee <programmer.and.investor@gmail.com>
2024-04-15 16:19:58 -0300

    style: add stylesheet attributes to InfoCard component
    Co-authored-by: PauloBezz <paulo.bezerra2057@gmail.com>

--- src/components/cards/InfoCard/index.jsx
+++ src/components/cards/InfoCard/index.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import styles from "./style.module.css"
 
-export function InfoCard({title, number, men, women, grafico, info}){
+export function InfoCard({ title, number, men, women, grafico, info }) {
 
-    return(
-        <>
-        <div className={styles}>
-            <h3>{title}</h3>
-            <h1>{number}</h1>
-            <h4>{men}</h4>
-            <h4>{women}</h4>
+    return (
+        <div className={styles.container}>
+            <div>
+                <h2 className={styles.title}>{title}</h2>
+                <h1 className={styles.number}>{number}</h1>
+                <div className={styles.sexContainer}>
+                    <span className={styles.men}>{men}</span>
+                    <span className={styles.women}>{women}</span>
+                </div>
+            </div>
+            
+            <div>
+                <div>{grafico}</div>
+                <span>{info}</span>
+            </div>
         </div>
-        <div>
-            <div>{grafico}</div>
-            <h4>{info}</h4>
-        </div>
-        </>
     )
 }
