f7b936d2281daf2c3c8a0c9f692df69028debb5a
PauloBezz <paulo.bezerra2057@gmail.com>
2024-04-15 17:25:04 -0300

    feat: add Announcement component
    Co-authored-by: samuelleitee <programmer.and.investor@gmail.com>

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Sidebar } from "../../components/Sidebar";
 import { ResourceCard } from "../../components/cards/ResourceCards"
 import { InfoCard } from "../../components/cards/InfoCard";
+import { Announcement } from "../../components/Announcement";
 
 export function Dashboard() {
     return (
@@ -12,6 +13,7 @@ export function Dashboard() {
         <ResourceCard title="New Employees" number="24" info="4 Departament" />
         <InfoCard title={"Total Employees"} number="216" men="120 Men" women="96 Women" grafico="Gráfico Aqui" info="+2 Past month" />
         <InfoCard title={"Talent Request"} number="16" men="6 Men" women="10 Women" grafico="Gráfico Aqui" info="+5% Past month" />
+        <Announcement message={"Outing schedule for every departement"} time={"5 Minutes ago"}/>
       </>
     );
   }
