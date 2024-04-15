637c85deb77d8fcc2be5e43745a73559d1758824
samuelleitee <programmer.and.investor@gmail.com>
2024-04-15 16:42:32 -0300

    style: add stylesheet attributes to ResourceCard component according to the variant defined
    Co-authored-by: PauloBezz <paulo.bezerra2057@gmail.com>

--- src/components/cards/ResourceCards/index.jsx
+++ src/components/cards/ResourceCards/index.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import styles from "./styles.module.css"
 
-export function ResourceCard({title, number, info}){
+export function ResourceCard({title, number, info, variant}){
+    function getVariant(variant) {
+        if (variant == "positions") {
+            return styles.positions
+        } else if (variant == "jobs") {
+            return styles.jobs
+        } else if (variant == "employees") {
+            return styles.employees
+        }
+    }
 
     return(
         <>
-        <div className={styles.container}>
+        <div className={`${styles.container} ${getVariant(variant)}`}>
             <h3>{title}</h3>
             <h1>{number}</h1>
             <h2>{info}</h2>
