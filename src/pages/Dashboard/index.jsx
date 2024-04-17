c5926169fab4c642e214bc7671d348b7453db70d
PauloBezz <paulo.bezerra2057@gmail.com>
2024-04-16 21:02:10 -0300

    fix: style finish in Announcement component
    Co-authored-by: samuelleitee <programmer.and.investor@gmail.com>

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -13,7 +13,9 @@ export function Dashboard() {
         <ResourceCard title="New Employees" number="24" info="4 Departament" variant="employees" />
         <InfoCard title={"Total Employees"} number="216" men="120 Men" women="96 Women" grafico="Gráfico Aqui" info="+2 Past month" />
         <InfoCard title={"Talent Request"} number="16" men="6 Men" women="10 Women" grafico="Gráfico Aqui" info="+5% Past month" />
-        <Announcement message={"Outing schedule for every departement"} time={"5 Minutes ago"}/>
+        <Announcement message={"Outing schedule for every departement"} time={"5 Minutes ago"} pinned/>
+        <Announcement message={"Meeting HR Department"} time={"Yesterday, 12:30 PM"}/>
+        <Announcement message={"IT Department need two more talents for UX/UI Designer position"} time={"Yesterday, 09:15 AM"}/>
       </>
     );
   }
