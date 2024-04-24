2c2868c3451c19c0e83e150a3fe4ff9b439360e2
PauloBezz <paulo.bezerra2057@gmail.com>
2024-04-23 21:30:32 -0300

    feat: create menu for tablet and mobile
    Co-authored-by: samuelleitee <programmer.and.investor@gmail.com>

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,11 +4,23 @@ import message from "../../assets/ic_message.svg";
 import profile from "../../assets/ic_profile.svg";
 import arrow from "../../assets/ic_arrow.svg";
 import styles from "./styles.module.css";
+import search from "./../../assets/ic_search.svg"
 
 export function Header({ nome }) {
   return (
     <header className={styles.container}>
-      <input type="text" placeholder="Search" className={styles.input} />
+      <div className={styles.menudiv}>
+
+        <div className={styles.menu}>
+          <div className={styles.line}></div>
+          <div className={styles.line}></div>
+          <div className={styles.line}></div>
+        </div>
+
+        <img src={search} alt="" />
+
+        <input type="text" placeholder="Search" className={styles.input} />
+      </div>
 
       <div className={styles.profile}>
         <Link>
